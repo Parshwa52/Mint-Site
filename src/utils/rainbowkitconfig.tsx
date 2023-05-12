f60af43ca6b20b5a10fa3c86f1982d1f4cbea696
chiragbadhe <badhechirag633@gmail.com>
2023-05-12 22:17:54 +0530

    ft:bridge

--- src/utils/rainbowkitconfig.tsx
+++ src/utils/rainbowkitconfig.tsx
@@ -1,13 +1,13 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, lightTheme, Theme } from "@rainbow-me/rainbowkit";
-import { configureChains, createClient } from "wagmi";
+import { configureChains, createClient, mainnet } from "wagmi";
 import { polygon } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { Color } from "three";
 
 export const { chains, provider } = configureChains(
-  [polygon],
-  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID as string })]
+  [polygon, mainnet],
+  [alchemyProvider({ apiKey: "I3K5UoJriahPrauT3HRuT7FDdHdyE_Um" })]
 );
 
 export const { connectors } = getDefaultWallets({
@@ -26,42 +26,42 @@ export const customTheme: Theme = {
     accentColor: "#fff",
     accentColorForeground: "black",
     borderRadius: "medium",
-    overlayBlur: 'small',
+    overlayBlur: "small",
     fontStack: "system",
   }),
-//   colors: {
-//       connectButtonText: "#FFFFFF",
-//       accentColor: "",
-//       accentColorForeground: "",
-//       actionButtonBorder: "",
-//       actionButtonBorderMobile: "",
-//       actionButtonSecondaryBackground: "",
-//       closeButton: "",
-//       closeButtonBackground: "",
-//       connectButtonBackground: "",
-//       connectButtonBackgroundError: "",
-//       connectButtonInnerBackground: "",
-//       connectButtonTextError: "",
-//       connectionIndicator: "",
-//       downloadBottomCardBackground: "",
-//       downloadTopCardBackground: "",
-//       error: "",
-//       generalBorder: "",
-//       generalBorderDim: "",
-//       menuItemBackground: "",
-//       modalBackdrop: "",
-//       modalBackground: "",
-//       modalBorder: "",
-//       modalText: "",
-//       modalTextDim: "",
-//       modalTextSecondary: "",
-//       profileAction: "",
-//       profileActionHover: "",
-//       profileForeground: "",
-//       selectedOptionBorder: "",
-//       standby: ""
-//   },
+  //   colors: {
+  //       connectButtonText: "#FFFFFF",
+  //       accentColor: "",
+  //       accentColorForeground: "",
+  //       actionButtonBorder: "",
+  //       actionButtonBorderMobile: "",
+  //       actionButtonSecondaryBackground: "",
+  //       closeButton: "",
+  //       closeButtonBackground: "",
+  //       connectButtonBackground: "",
+  //       connectButtonBackgroundError: "",
+  //       connectButtonInnerBackground: "",
+  //       connectButtonTextError: "",
+  //       connectionIndicator: "",
+  //       downloadBottomCardBackground: "",
+  //       downloadTopCardBackground: "",
+  //       error: "",
+  //       generalBorder: "",
+  //       generalBorderDim: "",
+  //       menuItemBackground: "",
+  //       modalBackdrop: "",
+  //       modalBackground: "",
+  //       modalBorder: "",
+  //       modalText: "",
+  //       modalTextDim: "",
+  //       modalTextSecondary: "",
+  //       profileAction: "",
+  //       profileActionHover: "",
+  //       profileForeground: "",
+  //       selectedOptionBorder: "",
+  //       standby: ""
+  //   },
   fonts: {
-    body: "'Courier New', sans-serif" ,
+    body: "'Courier New', sans-serif",
   },
 };
