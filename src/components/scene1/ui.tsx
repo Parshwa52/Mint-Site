3460ae95c05b9fcd09eebbf49cd5b7e39d00a8e6
Dylan-Bartoli <58397413+Dylan-Bartoli@users.noreply.github.com>
2023-05-16 16:36:33 +0200

    Add ui

--- src/components/scene1/ui.tsx
+++ src/components/scene1/ui.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
-import topPanel from '@/assets/ui/world/top-panel.png'
-import midTopPanel from '@/assets/ui/1/mid-panel.png'
-import midPanel from '@/assets/ui/world/mid-panel.png'
-import lowerPanel from '@/assets/ui/world/lower-panel.png'
+import topPanelWorld from '@/assets/ui/world/top-panel.png'
+import midPanelWorld from '@/assets/ui/world/mid-panel.png'
+import lowerPanelWorld from '@/assets/ui/world/lower-panel.png'
+import topPanelSpace from '@/assets/ui/space/top-panel.png'
+import midPanelSpace from '@/assets/ui/space/mid-panel.png'
+import lowerPanelSpace from '@/assets/ui/space/lower-panel.png'
 import { useEffect } from 'react'
 import { gsap } from 'gsap'
 import { SoundButton } from '../soundButton'
@@ -32,17 +34,15 @@ export const UI = ({ visible }: { visible?: boolean }) => {
   return (
     <div
       id='ui'
-      className='ui-space'
       style={{
         opacity: visible ? 1 : 0,
       }}
     >
-      <div className='ui-container'>
-        <Image src={topPanel} alt='ui top' className='ui-part ui-top' />
-        {/* <Image src={midTopPanel} alt='ui mid' className='ui-part ui-top' /> */}
+      <div className='ui-container ui-space'>
+        <Image src={topPanelSpace} alt='ui top' className='ui-part ui-top' />
         <div className='ui-part ui-mid'>
           <div className='ui-img'>
-            <Image src={midPanel} alt='ui mid' className='ui-img-img' />
+            <Image src={midPanelSpace} alt='ui mid' className='ui-img-img' />
             <div className='ui-text'>
               <div id='text-1'>
                 <p className='phrase-1'>
@@ -94,7 +94,74 @@ export const UI = ({ visible }: { visible?: boolean }) => {
             </div>
           </div>
         </div>
-        <Image src={lowerPanel} alt='ui lower' className='ui-part ui-lower' />
+        <Image
+          src={lowerPanelSpace}
+          alt='ui lower'
+          className='ui-part ui-lower'
+        />
+      </div>
+
+      <div className='ui-container ui-world'>
+        <Image src={topPanelWorld} alt='ui top' className='ui-part ui-top' />
+        <div className='ui-part ui-mid'>
+          <div className='ui-img'>
+            <Image src={midPanelWorld} alt='ui mid' className='ui-img-img' />
+            <div className='ui-text'>
+              <div id='text-1'>
+                <p className='phrase-1'>
+                  <Split
+                    splitWords
+                    splitChars
+                    textToSplit='I have the perfect place for you.'
+                  />
+                </p>
+                <p className='phrase-2'>
+                  <Split
+                    splitWords
+                    splitChars
+                    textToSplit='Let me take you to the world of Pluto.'
+                  />
+                </p>
+              </div>
+              <ul
+                className='ui-text-sounds'
+                style={{
+                  opacity: visible ? 1 : 0,
+                }}
+              >
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+                <li />
+              </ul>
+            </div>
+            <div className='ui-sounds'>
+              <p>0x548g...8h6k</p>
+              <SoundButton />
+            </div>
+          </div>
+        </div>
+        <Image
+          src={lowerPanelWorld}
+          alt='ui lower'
+          className='ui-part ui-lower'
+        />
       </div>
     </div>
   )
