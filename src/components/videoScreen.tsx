ea8930bf5cb8969ae67a41c3d966bf048b9a6df2
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-05-29 23:53:12 +0530

    ft: final screen text color, shadow, blur

--- src/components/videoScreen.tsx
+++ src/components/videoScreen.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const VideoScreen: React.FC<Props> = ({}) => {
   return (
-    <div className='relative h-screen video'>
+    <div className="relative h-screen video">
       <div
-        className='absolute inset-0 bg-cover bg-center'
+        className="absolute inset-0 bg-cover bg-center"
         style={{
           backgroundImage:
             "url('https://www.bandisoft.com/honeycam/help/file_format/sample.webp')",
@@ -21,22 +21,30 @@ const VideoScreen: React.FC<Props> = ({}) => {
         </a>
       </div> */}
 
-      <div className='absolute items-center justify-center flex w-full mt-[130px] top-0 p-4 '>
+      <div className="absolute items-center justify-center flex w-full mt-[100px] top-0 p-4 ">
         <a
-          href='#'
-          className='mr-4 text-[26px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'
+          href="#"
+          className="mr-4 text-lg sm:text-xl md:text-3xl font-bold text-white"
+          style={{
+            textShadow: "3px 3px 20px black",
+          }}
         >
           Congrats #123, now, you are a Misfit.
         </a>
       </div>
 
-      <div className='absolute items-center justify-center flex w-full bottom-0 p-4 text-[22px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
-        <a href='#' className='mr-4'>
+      <div
+        className="absolute items-center justify-center flex w-full bottom-5 p-4 text-[22px] font-semibold text-white"
+        style={{
+          textShadow: "3px 3px 20px black",
+        }}
+      >
+        <a href="#">
           View on OpenSea
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default VideoScreen
+export default VideoScreen;
