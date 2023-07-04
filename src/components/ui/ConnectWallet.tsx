5e2b06123a1906fb640b903e1a4fe317f954c652
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-07-04 17:18:27 +0530

    ft: delegate cash button

--- src/components/ui/ConnectWallet.tsx
+++ src/components/ui/ConnectWallet.tsx
@@ -1,18 +1,74 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit'
-import React from 'react'
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import React from "react";
+
+import dynamic from "next/dynamic";
+import { rpc_ethereum, rpc_polygon } from "@/constants";
+
+// @ts-ignore
+const DelegateCashButton = dynamic(() => import("delegatecash-button-react"), {
+  ssr: false,
+});
 
 function ConnectWallet(): JSX.Element {
   return (
     <div>
-      <ConnectButton
+      {/* <ConnectButton
         showBalance={false}
         accountStatus={{
           smallScreen: 'avatar',
           largeScreen: 'full',
         }}
-      />
+      /> */}
+      <ConnectButton.Custom>
+        {({
+          account,
+          chain,
+          openChainModal,
+          openConnectModal,
+          authenticationStatus,
+          mounted,
+        }) => {
+          const ready = mounted && authenticationStatus !== "loading";
+          const connected =
+            ready &&
+            account &&
+            chain &&
+            (!authenticationStatus || authenticationStatus === "authenticated");
+
+          if (!connected) {
+            return (
+              <div
+                {...(!ready && {
+                  "aria-hidden": true,
+                })}
+              >
+                <button
+                  className={`auth-button`}
+                  onClick={() => openConnectModal()}
+                >
+                  Connect
+                </button>
+              </div>
+            );
+          }
+
+          if (chain.unsupported) {
+            openChainModal();
+          }
+
+          return (
+            <DelegateCashButton
+              rpcUrl={[rpc_ethereum, rpc_polygon]}
+              onButtonClick={(event: any) => console.log(event.detail)}
+              onWalletSelect={(event: any) => console.log(event.detail)}
+            >
+              Connect
+            </DelegateCashButton>
+          );
+        }}
+      </ConnectButton.Custom>
     </div>
-  )
+  );
 }
 
-export default ConnectWallet
+export default ConnectWallet;
