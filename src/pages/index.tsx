60837adf2c75510874f5b46e39191f24b502d199
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-07-13 00:09:30 +0530

    chore: remove test button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,6 @@ import { useUIContext } from "@/provider/uiProvider";
 import MintModal from "@/components/ui/MintModal";
 
 export default function Home() {
-  const { setModalOpen } = useUIContext();
-
   return (
     <PageLayout
       pageTitle="Pluto Mint Website"
@@ -25,14 +23,6 @@ export default function Home() {
       {/* <SoundButton /> */}
 
       <MintModal />
-
-      {/* Testing */}
-      <button
-        onClick={() => setModalOpen(true)}
-        className="fixed top-5 right-0 z-50"
-      >
-        Open Mint Modal
-      </button>
     </PageLayout>
   );
 }
