0ceacad05ed598eb109ba58c356a43dd36f2f1c2
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-07-13 05:03:58 +0530

    ft: txn receipt

--- src/components/videoScreen.tsx
+++ src/components/videoScreen.tsx
@@ -42,7 +42,7 @@ const VideoScreen: React.FC<Props> = ({ tokenId }) => {
           href="#"
           className="mr-4 text-lg sm:text-xl md:text-3xl font-bold text-purple-200"
           style={{
-            textShadow: "2px 2px 10px #222",
+            textShadow: "2px 2px 10px #111",
           }}
         >
           Congrats #{tokenId}, now, you are a Misfit.
@@ -50,7 +50,12 @@ const VideoScreen: React.FC<Props> = ({ tokenId }) => {
       </div>
 
       {chainId === secondaryChainId ? (
-        <div className="text-purple-200">
+        <div
+          className="absolute bottom-20 left-[30%] text-purple-200 font-semibold"
+          style={{
+            textShadow: "2px 2px 10px #111",
+          }}
+        >
           Your NFT will be minted in approximately 10 minutes or less on
           Polygon!
         </div>
@@ -61,11 +66,14 @@ const VideoScreen: React.FC<Props> = ({ tokenId }) => {
       <div
         className="absolute space-x-5 justify-center flex w-full bottom-5 p-4 text-[22px] font-semibold text-purple-200"
         style={{
-          textShadow: "2px 2px 10px #222",
+          textShadow: "2px 2px 10px #111",
         }}
       >
         {chainId === primaryChainId ? (
-          <a href="" target="_blank">
+          <a
+            href={`https://magiceden.io/item-details/polygon/0xa131b877B12B0Ae8BB7da7229b8a1095881497A6/${tokenId}`}
+            target="_blank"
+          >
             View on MagicEden
           </a>
         ) : (
