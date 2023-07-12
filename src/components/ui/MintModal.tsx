5ad90f6eda7024ce1831e34c1ebc43e678b8c625
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-07-13 00:08:51 +0530

    fix: single quote

--- src/components/ui/MintModal.tsx
+++ src/components/ui/MintModal.tsx
@@ -90,7 +90,7 @@ export default function MintModal({}: Props) {
                       className="mt-5 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={closeModal}
                     >
-                      Let's go!
+                      Let&apos;s go!
                     </button>
                     {/* {modalData.max} |
                     {modalData.tokensToMint} */}
