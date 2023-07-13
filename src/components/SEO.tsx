77ba17e1e6b8defcbfe48762fd1677fb9c0b6065
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-07-13 08:39:06 +0530

    ft: meta image and url

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,23 +1,23 @@
 // React
-import Head from 'next/head'
-import { useRouter } from 'next/router'
+import Head from "next/head";
+import { useRouter } from "next/router";
 // style
-import colors from '@/styles/exporters/_colorExporter.module.sass'
+import colors from "@/styles/exporters/_colorExporter.module.sass";
 
 interface SEOProps {
-  title: string
-  desc: string
+  title: string;
+  desc: string;
 }
 
 const SEO = (props: SEOProps) => {
   // Get actual path
-  const path = useRouter().asPath
-  const url = 'https://www.pluto.xyz'
+  const path = useRouter().asPath;
+  const url = "https://www.mint.pluto.website";
   // Define SEO info
-  const SEOTitle = props.title
-  const SEODesc = props.desc
-  const SEORoute = url + path
-  const SEOThemeColor = colors.primary
+  const SEOTitle = props.title;
+  const SEODesc = props.desc;
+  const SEORoute = url + path;
+  const SEOThemeColor = colors.primary;
 
   function generateSchemaOrg() {
     return {
@@ -29,51 +29,51 @@ const SEO = (props: SEOProps) => {
         "url": "${url},
         "logo": ""
       }`,
-    }
+    };
   }
 
   return (
     <Head>
       <title>{SEOTitle}</title>
-      <meta name='title' content={SEOTitle} />
-      <meta name='description' content={SEODesc} />
+      <meta name="title" content={SEOTitle} />
+      <meta name="description" content={SEODesc} />
       {/* Viewport metatags */}
       <meta
-        name='viewport'
-        content='width=device-width, initial-scale=1, shrink-to-fit=no'
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
-      <meta httpEquiv='content-type' content='text/html; charset=utf-8' />
+      <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
       {/* Favicons metatags */}
-      <link rel='icon' type='image/png' sizes='16x16' href='/favicon.png' />
-      <link rel='icon' type='image/png' sizes='32x32' href='/favicon.png' />
-      <link rel='apple-touch-icon' sizes='180x180' href='/favicon.png' />
-      <link rel='mask-icon' color={SEOThemeColor} href='/favicon.png' />
-      <meta name='msapplication-TileColor' content={SEOThemeColor} />
-      <meta name='theme-color' content={SEOThemeColor} />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+      <link rel="mask-icon" color={SEOThemeColor} href="/favicon.png" />
+      <meta name="msapplication-TileColor" content={SEOThemeColor} />
+      <meta name="theme-color" content={SEOThemeColor} />
       {/* Social metatags */}
-      <meta property='og:type' content='website' />
-      <meta property='og:url' content={SEORoute} />
-      <meta property='og:title' content={SEOTitle} />
-      <meta property='og:site_name' content={SEOTitle} />
-      <meta property='og:description' content={SEODesc} />
-      <meta property='og:image' content='/icons/social-card.jpg' />
-      <meta property='twitter:card' content='summary_large_image' />
-      <meta property='twitter:url' content={SEORoute} />
-      <meta property='twitter:title' content={SEOTitle} />
-      <meta property='twitter:description' content={SEODesc} />
-      <meta property='twitter:image' content='/icons/social-card.jpg' />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={SEORoute} />
+      <meta property="og:title" content={SEOTitle} />
+      <meta property="og:site_name" content={SEOTitle} />
+      <meta property="og:description" content={SEODesc} />
+      <meta property="og:image" content="/assets/socialbanner.png" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={SEORoute} />
+      <meta property="twitter:title" content={SEOTitle} />
+      <meta property="twitter:description" content={SEODesc} />
+      <meta property="twitter:image" content="/assets/socialbanner.png" />
       {/* Crawler metatags */}
-      <link rel='canonical' href={SEORoute} key='canonical' />
+      <link rel="canonical" href={SEORoute} key="canonical" />
       {/* <link rel="manifest" href="/manifest.json" /> */}
-      <meta name='robots' content='index,follow' />
-      <meta name='google' content='nositelinkssearchbox' />
-      <meta name='revisit-after' content='7' />
+      <meta name="robots" content="index,follow" />
+      <meta name="google" content="nositelinkssearchbox" />
+      <meta name="revisit-after" content="7" />
       <script
-        type='application/ld+json'
+        type="application/ld+json"
         dangerouslySetInnerHTML={generateSchemaOrg()}
       />
     </Head>
-  )
-}
+  );
+};
 
-export default SEO
+export default SEO;
